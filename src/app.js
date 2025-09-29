3280225f57271645c07c34e4ea548048e27199b9
nabilabakhtawar <nabilabakhtawar@gmail.com>
2025-09-29 21:01:59 +0500

    Get & Display Posts

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
-// CommonJS Module Syntax
-// const person = require('./mymodule1');
+import { http } from './http';
+import { ui } from  './ui ' ;
+document.addEventListener('DOMContentLoaded',getposts);
+ function getposts(){
+    http.get('http:// localhosts:3000/posts')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
 
+ }
